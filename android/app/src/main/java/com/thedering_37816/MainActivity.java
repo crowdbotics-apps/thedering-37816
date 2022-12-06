ebb30762893ea7071a7746613daf5eedd440d2c3
Crowdbotics <team@crowdbotics.com>
2022-12-06 20:37:08 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/thedering_37816/MainActivity.java
@@ -0,0 +1,15 @@
+package com.thedering_37816;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "thedering_37816";
+  }
+}
